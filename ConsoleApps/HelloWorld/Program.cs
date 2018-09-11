d80672ad595b56e4dc8bfa103265d05aa45176ca
Steve Baer <steve@mcneel.com>
2018-09-10 18:55:24 -0700

    Add HelloWorld console application

---
+++ ConsoleApps/HelloWorld/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Rhino.Geometry;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        RhinoLib.LaunchInProcess(RhinoLib.LoadMode.Headless, 0);
+        MeshABrep();
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+      Console.WriteLine("press any key to exit");
+      Console.ReadKey();
+      RhinoLib.ExitInProcess();
+    }
+
+    // Place calls to RhinoCommon in a separate function from main so the jit doesn't
+    // try to load RhinoCommon until after RhinoLib.LaunchInProcess is called
+    static void MeshABrep()
+    {
+      var sphere = new Sphere(Point3d.Origin, 12);
+      var brep = sphere.ToBrep();
+      var mp = new MeshingParameters(0.5);
+      var mesh = Mesh.CreateFromBrep(brep, mp);
+      Console.WriteLine($"Mesh with {mesh[0].Vertices.Count} vertices created");
+    }
+  }
+}
---
+++ ConsoleApps/RhinoLib.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class RhinoLib
+{
+    public enum LoadMode : int
+    {
+        Headless = 0,
+        FullUserInterface = 1
+    }
+    static RhinoLib()
+    {
+        Init();
+    }
+
+    private static bool _pathsSet = false;
+    public static string _rhpath;
+    public static void Init()
+    {
+        if (!_pathsSet)
+        {
+            string envPath = Environment.GetEnvironmentVariable("path");
+
+            // Add Rhino WIP to the path.
+            string programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+            string rhinoSystemDir = System.IO.Path.Combine(programFiles, "Rhino WIP", "System");
+
+            Environment.SetEnvironmentVariable("path", envPath + ";" + rhinoSystemDir);
+            _pathsSet = true;
+            _rhpath = rhinoSystemDir;
+            _rhinocommon = System.Reflection.Assembly.LoadFrom(System.IO.Path.Combine(rhinoSystemDir, "RhinoCommon.dll"));
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+        }
+    }
+
+    static System.Reflection.Assembly _rhinocommon;
+    private static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+    {
+        if (args.Name.Contains("RhinoCommon"))
+            return _rhinocommon;
+        return null;
+    }
+
+    [DllImport("RhinoLibrary.dll")]
+    internal static extern int LaunchInProcess(LoadMode mode, int reserved2);
+
+    [DllImport("RhinoLibrary.dll")]
+    internal static extern int ExitInProcess();
+}
