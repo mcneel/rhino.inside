cb73e0c38e96761bec04548f005274995607dab8
Kike <kike@mcneel.com>
2019-03-27 15:40:51 +0100

    Category param is now a PersistentParam.

--- Autodesk/Revit/GH/Category/Category.cs
+++ Autodesk/Revit/GH/Category/Category.cs
@@ -84,6 +84,67 @@ namespace RhinoInside.Revit.GH.Types
       return base.CastTo<Q>(ref target);
     }
 
+    class Proxy : IGH_GooProxy
+    {
+      public Proxy(Category owner) { proxyOwner = owner; (this as IGH_GooProxy).UserString = FormatInstance(); }
+
+      public void Construct() { }
+      public string MutateString(string str) => str.Trim();
+      public string FormatInstance()
+      {
+        int value = proxyOwner.Value.IntegerValue;
+        if (Enum.IsDefined(typeof(Autodesk.Revit.DB.BuiltInCategory), value))
+          return ((BuiltInCategory)value).ToString();
+
+        return value.ToString();
+      }
+      public bool FromString(string str)
+      {
+        if (Enum.TryParse(str, out Autodesk.Revit.DB.BuiltInCategory builtInCategory))
+        {
+          proxyOwner.Value = new ElementId(builtInCategory);
+          return true;
+        }
+
+        return false;
+      }
+
+      readonly Category proxyOwner;
+      IGH_Goo IGH_GooProxy.ProxyOwner => proxyOwner;
+      bool IGH_GooProxy.IsParsable => true;
+      string IGH_GooProxy.UserString { get; set; }
+
+      Autodesk.Revit.DB.Category category => proxyOwner.Value != null && Revit.ActiveDBDocument != null ? Autodesk.Revit.DB.Category.GetCategory(Revit.ActiveDBDocument, proxyOwner.Value) : null;
+
+      public bool Valid => category != null;
+      [System.ComponentModel.Description("The category identifier in this session.")]
+      public int Id => proxyOwner.Value.IntegerValue;
+      [System.ComponentModel.Description("The category Name.")]
+      public string Name => category?.Name;
+
+      [System.ComponentModel.Category("Other"), System.ComponentModel.Description("Parent category of this category.")]
+      public string Parent => category?.Parent?.Name;
+      [System.ComponentModel.Category("Other"), System.ComponentModel.Description("Category can have project parameters.")]
+      public bool AllowsParameters => category?.AllowsBoundParameters ?? false;
+      [System.ComponentModel.Category("Other"), System.ComponentModel.Description("Identifies if the category is associated with a type of tag for a different category.")]
+      public bool IsTag => category?.IsTagCategory ?? false;
+
+      [System.ComponentModel.Category("Material"), System.ComponentModel.Description("Material of the category.")]
+      public string Material => category?.Material?.Name;
+      [System.ComponentModel.Category("Material"), System.ComponentModel.Description("Identifies if elements of the category are able to report what materials they contain in what quantities.")]
+      public bool HasMaterialQuantities => category?.HasMaterialQuantities ?? false;
+
+      [System.ComponentModel.Category("Geometry"), System.ComponentModel.Description("Category type of this category.")]
+      public CategoryType CategoryType => category?.CategoryType ?? CategoryType.Invalid;
+      [System.ComponentModel.Category("Geometry"), System.ComponentModel.Description("Indicates if the category is cuttable or not.")]
+      public bool IsCuttable => category?.IsCuttable ?? false;
+
+      [System.ComponentModel.Category("Display"), System.ComponentModel.Description("The color of lines shown for elements of this category.")]
+      public System.Drawing.Color LineColor => category?.LineColor.ToRhino() ?? System.Drawing.Color.Empty;
+    }
+
+    public override IGH_GooProxy EmitProxy() => new Proxy(this);
+
     public override sealed string ToString()
     {
       if (IsValid)
@@ -102,13 +163,16 @@ namespace RhinoInside.Revit.GH.Types
 
 namespace RhinoInside.Revit.GH.Parameters
 {
-  public class Category : GH_Param<Types.Category>
+  public class Category : GH_PersistentParam<Types.Category>
   {
     public override Guid ComponentGuid => new Guid("6722C7A5-EFD3-4119-A7FD-6C8BE892FD04");
     public override GH_Exposure Exposure => GH_Exposure.secondary;
     protected override System.Drawing.Bitmap Icon => ImageBuilder.BuildIcon("C");
 
-    public Category() : base("Category", "Category", "Represents a Revit document category.", "Revit", "Category", GH_ParamAccess.item) { }
+    public Category() : base("Category", "Category", "Represents a Revit document category.", "Revit", "Category") { }
+
+    protected override GH_GetterResult Prompt_Plural(ref List<Types.Category> values) => GH_GetterResult.cancel;
+    protected override GH_GetterResult Prompt_Singular(ref Types.Category value) => GH_GetterResult.cancel;
   }
 }
 
