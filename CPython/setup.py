650cfe89bd50dbab83b5c1c0dc5b3d0225522071
Steve Baer <steve@mcneel.com>
2019-09-03 16:20:08 -0700

    simplify pip package

--- CPython/setup.py
+++ CPython/setup.py
@@ -2,26 +2,19 @@ import setuptools
 import os
 import struct
 import distutils.command.install
+from os import path
 
-
-class _CustomInstall(distutils.command.install.install):
-    def run(self):
-        if os.name != 'nt':
-            raise SystemError('rhinoinside only works on Windows')
-        bitness = 8 * struct.calcsize("P")
-        if bitness != 64:
-            raise SystemError('rhinoinside only works in a 64 bit process')
-        distutils.command.install.install.run(self)
 
 # read the contents of your README file
-from os import path
 this_directory = path.abspath(path.dirname(__file__))
+long_description = ""
 with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
+
 setuptools.setup(
     name="rhinoinside",
-    version="0.4.2",
+    version="0.4.3",
     packages=['rhinoinside'],
     author="Robert McNeel & Associates",
     author_email="steve@mcneel.com",
@@ -36,6 +29,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: Microsoft :: Windows",
         "Programming Language :: Python"
-    ],
-    cmdclass={ 'install': _CustomInstall }
+    ]
 )
