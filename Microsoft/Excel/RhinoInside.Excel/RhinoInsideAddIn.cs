f0df5f397883b5698f24f8e4a9fff7eacd2a7bf1
David Andres Leon <45174266+davidandresleon@users.noreply.github.com>
2019-09-04 14:58:02 +0200

    Microsoft/office/wip (#158)
    * Added Rhino.Inside excel first test
    
    * created readme.md
    
    * updated instructions
    
    * udpated instructions
    
    * updated instructions for testing
    
    * updated instructions
    
    -added 64bits in excel
    -added VSTO Add-in extension for VS

---
+++ Microsoft/Excel/RhinoInside.Excel/RhinoInsideAddIn.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Reflection;
+using System.IO;
+
+using Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+
+using Rhino.Runtime.InProcess;
+
+namespace RhinoInside.Excel
+
+{
+    public partial class RhinoInsideAddIn
+    {
+        Rhino.Runtime.InProcess.RhinoCore m_rhino_core;
+
+        #region Plugin static constructor
+
+        static RhinoInsideAddIn()
+        {
+
+            ResolveEventHandler OnRhinoCommonResolve = null;
+            AppDomain.CurrentDomain.AssemblyResolve += OnRhinoCommonResolve = (sender, args) =>
+            {
+                const string rhino_common_assembly_name = "RhinoCommon";
+                var assembly_name = new AssemblyName(args.Name).Name;
+
+                if (assembly_name != rhino_common_assembly_name)
+                    return null;
+
+                AppDomain.CurrentDomain.AssemblyResolve -= OnRhinoCommonResolve;
+
+                var rhino_system_dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "Rhino WIP", "System");
+                return Assembly.LoadFrom(Path.Combine(rhino_system_dir, rhino_common_assembly_name + ".dll"));
+            };
+        }
+
+        #endregion // Plugin static constructor
+
+        //The Startup event is raised after the VSTO Add-in is loaded
+        //and all the initialization code in the assembly has been run. 
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            // Load Rhino
+            try
+            {
+
+
+                //TODO >> grab Excel product name for scheme
+                var scheme_name = "RhinoInsideExcel";
+
+
+                //Run Rhino.Inside no UI
+                m_rhino_core = new Rhino.Runtime.InProcess.RhinoCore(new[] { $"/scheme={scheme_name}" });
+
+                //TODO >> Run Rhino.Inside with UI - Error -200
+                //m_rhino_core = new Rhino.Runtime.InProcess.RhinoCore(new[] { $"/scheme={scheme_name}" }, WindowStyle.Maximized);
+
+
+            }
+            catch (Exception ex)
+            {
+                //System.Windows.Forms.MessageBox.Show(ex.Message);
+                System.Windows.Forms.MessageBox.Show("Cannot load Rhino Inside Addin");
+
+            }
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            try
+            {
+                m_rhino_core?.Dispose();
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+
+
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
---
+++ Microsoft/Excel/readme.md
@@ -0,0 +1,20 @@
+# Rhino Inside Excel
+This folder contains a test sample for using Rhino Inside Microsoft Excel using Visual Studio Tools for Office (VSTO).
+
+## Getting started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes
+
+### Prerequisites
+1. Microsoft Excel 2016 **64bits**
+2. Visual Studio 2017 ([download](https://visualstudio.microsoft.com/downloads/)) with VSTO Add-in extension
+3. Rhino WIP ([download](https://www.rhino3d.com/download/rhino/wip))
+
+### Getting Source and testing
+1. Clone the repository. At the command prompt, enter the following command:
+```
+git clone --recursive https://github.com/mcneel/rhino.inside.git rhino.inside
+```
+2. In Visual Studio: open _rhino.inside\Microsoft\Excel\RhinoInside.Excel.sln.
+3. Press *Start* to run the debugger.
+4. In Excel, go to the Add-ins tab
+5. In the *Rhino Inside* group, press the *test01* button.
