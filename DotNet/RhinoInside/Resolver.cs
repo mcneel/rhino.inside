a3e497c0a30d5723ad528f86fd6ec5ced80b074f
Steve Baer <steve@mcneel.com>
2019-09-27 19:20:22 -0700

    create .NET RhinoInside project

---
+++ DotNet/RhinoInside/Resolver.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Reflection;
+
+namespace RhinoInside
+{
+  public class Resolver
+  {
+    /// <summary>
+    /// Set up an assembly resolver to load RhinoCommon and other Rhino
+    /// assemblies from where Rhino is installed
+    /// </summary>
+    public static void Initialize()
+    {
+      if (System.IntPtr.Size != 8)
+        throw new Exception("Only 64 bit applications can use RhinoInside");
+      AppDomain.CurrentDomain.AssemblyResolve += ResolveForRhinoAssemblies;
+    }
+
+    static string _rhinoSystemDirectory;
+
+    /// <summary>
+    /// Directory used by assembly resolver to attempt load core Rhino assemblies. If not manually set,
+    /// this will be determined by inspecting the registry
+    /// </summary>
+    public static string RhinoSystemDirectory
+    {
+      get
+      {
+        if(string.IsNullOrWhiteSpace(_rhinoSystemDirectory))
+        {
+          string baseName = @"SOFTWARE\McNeel\Rhinoceros";
+          using (var baseKey = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(baseName))
+          {
+            string[] children = baseKey.GetSubKeyNames();
+            Array.Sort(children);
+            string versionName = "";
+            for(int i=children.Length-1; i>=0; i--)
+            {
+              if(double.TryParse(children[i], out double d))
+              {
+                versionName = children[i];
+                using (var installKey = baseKey.OpenSubKey($"{versionName}\\Install"))
+                {
+                  string corePath = installKey.GetValue("CoreDllPath") as string;
+                  if( System.IO.File.Exists(corePath))
+                  {
+                    _rhinoSystemDirectory = System.IO.Path.GetDirectoryName(corePath);
+                    break;
+                  }
+                }
+              }
+            }
+          }
+        }
+        return _rhinoSystemDirectory;
+      }
+      set
+      {
+        _rhinoSystemDirectory = value;
+      }
+    }
+
+    static Assembly ResolveForRhinoAssemblies(object sender, ResolveEventArgs args)
+    {
+      var assemblyName = new AssemblyName(args.Name).Name;
+      string path = System.IO.Path.Combine(RhinoSystemDirectory, assemblyName + ".dll");
+      if (System.IO.File.Exists(path))
+        return Assembly.LoadFrom(path);
+
+      return null;
+    }
+  }
+}
