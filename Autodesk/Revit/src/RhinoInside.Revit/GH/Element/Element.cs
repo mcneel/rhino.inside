de9c243d1960953d2c2071ebaaaa35337af21c51
Kike <kike@mcneel.com>
2019-09-28 21:42:03 +0200

    Added ElementMaterials and MaterialQuanitiesByElement

--- Autodesk/Revit/src/RhinoInside.Revit/GH/Element/Element.cs
+++ Autodesk/Revit/src/RhinoInside.Revit/GH/Element/Element.cs
@@ -913,6 +913,36 @@ namespace RhinoInside.Revit.GH.Components
     }
   }
 
+  public class ElementMaterials : Component
+  {
+    public override Guid ComponentGuid => new Guid("93C18DFD-FAAB-4CF1-A681-C11754C2495D");
+
+    public ElementMaterials()
+    : base("Element.Materials", "Element.Materials", "Query element used materials", "Revit", "Element")
+    { }
+
+    protected override void RegisterInputParams(GH_InputParamManager manager)
+    {
+      manager.AddParameter(new Parameters.Element(), "Element", "E", "Element to query for its materials", GH_ParamAccess.item);
+    }
+
+    protected override void RegisterOutputParams(GH_OutputParamManager manager)
+    {
+      manager.AddParameter(new Parameters.Material(), "Materials", "M", "Materials this Element is made of", GH_ParamAccess.list);
+      manager.AddParameter(new Parameters.Material(), "Paint", "P", "Materials used to paint this Element", GH_ParamAccess.list);
+    }
+
+    protected override void SolveInstance(IGH_DataAccess DA)
+    {
+      Autodesk.Revit.DB.Element element = null;
+      if (!DA.GetData("Element", ref element))
+        return;
+
+      DA.SetDataList("Materials", element?.GetMaterialIds(false).Select(x => element.Document.GetElement(x)));
+      DA.SetDataList("Paint",     element?.GetMaterialIds( true).Select(x => element.Document.GetElement(x)));
+    }
+  }
+
   public class ElementDelete : TransactionalComponent
   {
     public override Guid ComponentGuid => new Guid("213C1F14-A827-40E2-957E-BA079ECCE700");
