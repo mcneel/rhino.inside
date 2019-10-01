b6c4886d4c956b4341d10e0c1b2e46c992c92155
Kike <kike@mcneel.com>
2019-10-01 19:40:10 +0200

    Moved GH/Material/Material.cs to Types/Material.cs

--- Autodesk/Revit/src/RhinoInside.Revit/GH/Components/MaterialQuanitiesByElement.cs
+++ Autodesk/Revit/src/RhinoInside.Revit/GH/Components/MaterialQuanitiesByElement.cs
@@ -1,37 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using Autodesk.Revit.DB;
-using Autodesk.Revit.UI.Selection;
 using Grasshopper.Kernel;
-using Grasshopper.Kernel.Types;
-
-namespace RhinoInside.Revit.GH.Types
-{
-  public class Material : Element
-  {
-    public override string TypeName => "Revit Material";
-    public override string TypeDescription => "Represents a Revit material";
-    protected override Type ScriptVariableType => typeof(Autodesk.Revit.DB.Material);
-    public static explicit operator Autodesk.Revit.DB.Material(Material self) => Revit.ActiveDBDocument?.GetElement(self) as Autodesk.Revit.DB.Material;
-
-    public Material() { }
-    public Material(Autodesk.Revit.DB.Material material) : base(material) { }
-  }
-}
-
-namespace RhinoInside.Revit.GH.Parameters
-{
-  public class Material : ElementIdNonGeometryParam<Types.Material, Autodesk.Revit.DB.Material>
-  {
-    public override GH_Exposure Exposure => GH_Exposure.tertiary;
-    public override Guid ComponentGuid => new Guid("B18EF2CC-2E67-4A5E-9241-9010FB7D27CE");
-
-    public Material() : base("Material", "Material", "Represents a Revit document material.", "Params", "Revit") { }
-  }
-}
-
 
 namespace RhinoInside.Revit.GH.Components
 {
