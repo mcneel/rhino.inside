372b6215478e79b66352be36388f05824f2db899
Kike <kike@mcneel.com>
2019-10-29 10:56:14 +0100

    Update RhinoCommon dependency version to 7.0.19183.10495-wip

--- Autodesk/Revit/src/RhinoInside.Revit/Addin.cs
+++ Autodesk/Revit/src/RhinoInside.Revit/Addin.cs
@@ -22,7 +22,7 @@ namespace RhinoInside.Revit
 
     internal static readonly string RhinoExePath = Path.Combine(SystemDir, "Rhino.exe");
     internal static readonly FileVersionInfo RhinoVersionInfo = File.Exists(RhinoExePath) ? FileVersionInfo.GetVersionInfo(RhinoExePath) : null ;
-    static readonly Version MinimumRhinoVersion = new Version(7, 0, 19183);
+    static readonly Version MinimumRhinoVersion = new Version(7, 0, 19274);
     static readonly Version RhinoVersion = new Version
     (
       RhinoVersionInfo?.FileMajorPart ?? 0,
