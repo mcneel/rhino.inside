e800e74219545c5c54b89ac098ca5fdeef70c15c
Ehsan Iran-Nejad <eirannejad@gmail.com>
2024-07-16 15:31:25 -0700

    added base rhinocode test to unit test

--- DotNet/RhinoInside.Tests/ResolverTests.cs
+++ DotNet/RhinoInside.Tests/ResolverTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 
+using Rhino.Runtime.InProcess;
 using NUnit.Framework;
 
 namespace RhinoInside.Tests
@@ -9,9 +10,23 @@ namespace RhinoInside.Tests
   [TestFixture]
   public class ResolverTests
   {
+    [OneTimeSetUp]
+    public void OneTimeSetUpRhinoCore()
+    {
+      string rhinoPath = Environment.GetEnvironmentVariable("RHINOINSIDE_RHINOPATH");
+      Resolver.Initialize(rhinoPath);
+    }
+
     [Test]
     public void TestRhinoCore()
     {
+      RhinoCoreUtils.RunRhinoCore();
+    }
+
+    [Test]
+    public void TestMeshFromBrep()
+    {
+      Assert.Ignore();
       var p = RunRhinoInsideTest("test_MeshFromBrep");
       Assert.AreEqual(0, p.ExitCode);
     }
@@ -30,6 +45,16 @@ namespace RhinoInside.Tests
       var p = Process.Start(pinfo);
       p.WaitForExit();
       return p;
+    }
+  }
+
+  static class RhinoCoreUtils
+  {
+    public static void RunRhinoCore()
+    {
+      using (new RhinoCore())
+      {
+      }
     }
   }
 }
