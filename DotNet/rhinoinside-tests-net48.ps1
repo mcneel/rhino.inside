dd93121faa9151154702c9d41b21cc60b3329305
eirannejad <eirannejad@gmail.com>
2025-05-08 11:54:06 -0700

    Added arg check to test scripts

--- DotNet/rhinoinside-tests-net48.ps1
+++ DotNet/rhinoinside-tests-net48.ps1
@@ -1,3 +1,9 @@
+# — Check whether first positional argument was supplied
+if (-not $args.Count -or [string]::IsNullOrWhiteSpace($args[0])) {
+    $self = Split-Path -Leaf $PSCommandPath  # script’s own filename
+    Write-Host "Usage:`n  $self <rhino_system_path>" -ForegroundColor Red
+    exit 1
+}
 $rhino=$args[0]
 $env:RHINOINSIDE_RHINOPATH=$rhino
 Write-Host "RHINOINSIDE_RHINOPATH = " -nonewline
