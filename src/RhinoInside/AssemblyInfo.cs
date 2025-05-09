b00e9be6a8093ddbc3a3f8b2b9794171a81a3ad7
eirannejad <eirannejad@gmail.com>
2025-05-09 09:49:48 -0700

    Fixed strong-naming test assembly

--- src/RhinoInside/AssemblyInfo.cs
+++ src/RhinoInside/AssemblyInfo.cs
@@ -1,1 +1,7 @@
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("RhinoInside.Tests")]
+using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("RhinoInside.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100e5edffc7f30785" +
+                                                  "1226fa09faf1bb0de28bd160c4faf04ee28bcb33519ae23c3be02b6a582cd29a551b01bb90ec71" +
+                                                  "b7bc526278f92caf68efbaec953ef14dd4f0429b8b72ae3b9853a590d04afa4c978e9990997df4" +
+                                                  "81d782a4ad2c328f15660c77105925b3ac2d6b86f2f246ae7e57fe0a69fb656f1deaaaa2e648d7" +
+                                                  "b79bc7ce")]
+
